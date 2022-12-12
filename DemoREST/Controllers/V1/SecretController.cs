e1fa613161e065edd4f994a220c33b9e29690434
Subal Chatterjee <subalchatterjee1997@gmail.com>
2022-12-12 22:31:11 +0530

    Added Api-Key based authN for secret service. Use correct db connection.

---
+++ DemoREST/Controllers/V1/SecretController.cs
@@ -0,0 +1,15 @@
+﻿using DemoREST.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoREST.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("Now you have all the secrets");
+        }
+    }
+}
