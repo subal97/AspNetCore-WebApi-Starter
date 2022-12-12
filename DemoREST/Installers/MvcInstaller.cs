f88df95cf52286e92595fbb2caced860905b0142
Subal Chatterjee <subalchatterjee1997@gmail.com>
2022-12-12 21:21:22 +0530

    Refactor for Swagger Installation. Added fluent validation for Create/Update post request objects

--- DemoREST/Installers/MvcInstaller.cs
+++ DemoREST/Installers/MvcInstaller.cs
@@ -66,34 +66,6 @@ namespace DemoREST.Installers
             });
 
             services.AddSingleton<IAuthorizationHandler, HasOrganizationEmailHandler>();
-
-
-
-            services.AddSwaggerGen(x =>
-            {
-                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API service", Version = "v1" });
-
-                x.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "bearer",
-                    BearerFormat = "JWT",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Description = "JET Authorization header using the bearer scheme",
-                });
-
-                x.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference{Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
-                        },
-                        new string[]{ }
-                    }
-                });
-            });
         }
     }
 }
---
+++ DemoREST/Validators/CreatePostRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using DemoREST.Contracts.V1.Requests;
+using FluentValidation;
+
+namespace DemoREST.Validators
+{
+    public class CreatePostRequestValidator : AbstractValidator<CreatePostRequest>
+    {
+        public CreatePostRequestValidator()
+        {
+            RuleForEach(x => x.Tags).SetValidator(new TagRequestValidator());
+        }
+    }
+}
